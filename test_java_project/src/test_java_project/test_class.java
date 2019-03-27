7e1bd5b74ad48aa91229489ec9d9941b75f6d2a0
swanilee <ssunkyung00@gmail.com>
2019-03-27 11:35:34 +0900

    merge tool not access

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -2,10 +2,7 @@ package test_java_project;
 public class test_class {
 	public static void main(String[] args) {
 		System.out.println("hello world~! hoftix~!");
-<<<<<<< HEAD
 		System.out.println("I'm the master");
-=======
 		System.out.println("I'm the hotfix~!");
->>>>>>> refs/heads/hotfix
 	}
 }
