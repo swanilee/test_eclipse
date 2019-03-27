9fa8d7f41c470af861ac69400cf055a3209c0888
swanilee <ssunkyung00@gmail.com>
2019-03-27 11:26:20 +0900

    master branch change a code

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -2,5 +2,6 @@ package test_java_project;
 public class test_class {
 	public static void main(String[] args) {
 		System.out.println("hello world~! hoftix~!");
+		System.out.println("I'm the master");
 	}
 }
